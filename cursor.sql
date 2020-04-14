160a876ad533d9f31c1cb3fbb7bc281950b4f197
jhen852 <jhenny.callapa.quispe@gmail.com>
2020-04-13 23:20:55 -0400

    version1

--- cursor.sql
+++ cursor.sql
@@ -1,4 +1,40 @@
---mostrar con un cursor las plataformas y la cantidad de estudiantes y profesores que estan en ella 
-
-declare 
+--Mostrar a los 5 primeros profesores con mayor experiencia
+declare
+nm varchar2(50); 
+nc varchar2(50); 
+    cursor cr1 is
+        select p.nombre, p.experiencia
+        from profesor p order by p.experiencia desc;
+          
+   begin
     
+        open cr1;
+            for i in 1..5 loop
+               fetch cr1 into nm, nc;
+                exit when cr1%notfound;
+                dbms_output.put_line(nm||'los años  '||nc);
+                commit;
+            end loop;
+        close cr1;
+   end;
+ 
+--crear un cursor de cuantos mienbros del equipo tiene cada jefe de trabajo
+ create or replace procedure cr2 (x jefetrab.nombre%type)
+ as
+      cursor cr3
+      is
+            select j.nombre , s.NRO_INTEGRANTES
+                from jefetrab j, equipotrab s
+                    where j.ID_EQUIPO= s.ID_EQUIPO and j.nombre=x;
+
+begin 
+       for reg in cr3 loop
+            dbms_output.put_line(reg.nombre||'  Tiene '||reg.nro_integrantes ||'integrantes en su equipo de trabajo');
+       end loop;
+       
+end;
+            
+
+    begin
+            cr2('NICOLAS ALCAZAR HERAS');
+    end;
